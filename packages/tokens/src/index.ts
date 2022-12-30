634bb2e1d96629f978ef552204820aa532bd303c
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-12-30 11:03:14 -0300

    Initial commit

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,11 @@
+export * from './colors'
+
+// import { colors } from '#ignite-ui/tokens
+// instead of
+// import { colors } from '#ignite-ui/tokens/colors
+
+/*
+ * npx tsc --init
+ * npx tsc // to convert files
+ * 
+ */ 
