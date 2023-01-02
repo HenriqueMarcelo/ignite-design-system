c390ab9743eddd63e7732057a3956ad79000d175
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-01-02 09:26:58 -0300

    Creating Story Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,21 @@
+import type {StoryObj, Meta} from '@storybook/react'
+
+import { Button, ButtonsProps } from '@ignite-ui/react'
+
+export default {
+    title: "Button",
+    component: Button,
+
+    args: {
+        children: "Enviar"
+    }
+} as Meta<ButtonsProps>
+
+export const Primary: StoryObj<ButtonsProps> = {
+}
+
+export const Big: StoryObj<ButtonsProps> = {
+    args: {
+        size: "big"
+    }
+}
