c692c8760041abce3312535afd995c200ea8834d
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-01-03 09:19:08 -0300

    Component: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,22 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    children:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore at possimus cum ratione porro hic qui dignissimos animi totam quod, eveniet perspiciatis explicabo laboriosam quos velit repudiandae aliquid? Suscipit, odit!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustoTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
